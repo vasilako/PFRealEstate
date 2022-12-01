89805b90b5b1b9b01c11713195aea1935badfb6b
vasilako <vasilako@hotmail.com>
2022-12-01 11:46:17 +0100

    Created 3 apps, 'PFRealEstate.core_app', 'PFRealEstate.property_app', 'PFRealEstate.user_agent_app',

--- PFRealEstate/settings.py
+++ PFRealEstate/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+    'PFRealEstate.core_app',
+    'PFRealEstate.property_app',
+    'PFRealEstate.user_agent_app',
+
 ]
 
 MIDDLEWARE = [
---
+++ PFRealEstate/user_agent_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UserAgentAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'PFRealEstate.user_agent_app'
